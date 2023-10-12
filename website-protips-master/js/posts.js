4ce0fc3b350cf1fc4c45b0a993aa8ede0d4c24c9
Thales Fernandes <thalesinacioo@gmail.com>
2023-10-11 21:45:52 -0300

    Várias modificações
    Remoção de arquivos desnecessários
    Remoção newsletter em artigos.html
    Troca da cor no menu
    Adição de imagens dos artigos
    Remoção de todos os arquivos de imagens desnecessários
    Troca do titulo na seção artigos
    Remoção newsletter em linksuteis.html
    Remoção newsletter em news-detail.html

--- website-protips-master/js/posts.js
+++ website-protips-master/js/posts.js
@@ -1,27 +1,27 @@
 var posts = [
   {
-    "imagem": "https://picsum.photos/id/100/348/185",
+    "imagem": "imagens/01.jpg",
     "titulo": "Um título",
     "autor": "José Otavio",
     "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/100/1110/590",
+    "imagem_post": "imagens/01.jpg",
     "texto": "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus."
   },
   {
-    "imagem": "https://picsum.photos/id/200/348/185",
+    "imagem": "imagens/02.jpg",
     "titulo": "Um título 2",
     "autor": "José Otavio",
     "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/200/1110/590",
+    "imagem_post": "imagens/02.jpg",
     "texto": "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
     "video": "https://www.youtube.com/embed/y__ero7suPo"
   },
   {
-    "imagem": "https://picsum.photos/id/300/348/185",
+    "imagem": "imagens/03.jpg",
     "titulo": "Um título 3",
     "autor": "José Otavio",
     "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/300/1110/590",
+    "imagem_post": "imagens/03.jpg",
     "texto": "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
     "video": "https://www.youtube.com/embed/vvt2hgREqag"
   },
@@ -41,69 +41,6 @@ var posts = [
     "data": "02 Fev 2019",
     "imagem_post": "https://picsum.photos/id/55/1110/590",
     "texto": "5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/99/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/99/1110/590",
-    "texto": "6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/73/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/73/1110/590",
-    "texto": "7 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/85/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/85/1110/590",
-    "texto": "8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/91/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/91/1110/590",
-    "texto": "9 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/100/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/103/1110/590",
-    "texto": "10 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/111/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/111/1110/590",
-    "texto": "11 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/12/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/12/1110/590",
-    "texto": "12 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
     "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
   }
 ];
